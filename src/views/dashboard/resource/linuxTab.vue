ff8820e7e1517925028ed7a19044313bf88912de
feifeigood <baipengfeisw@163.com>
2021-09-23 10:24:51 +0800

    commit

--- src/views/dashboard/resource/linuxTab.vue
+++ src/views/dashboard/resource/linuxTab.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD:src/views/dashboard/resource/hostTab.vue
   <el-table style="width: 100%" v-loading="loading" :data="tableData">
     <el-table-column
       prop="hostname"
@@ -22,9 +21,6 @@
     <el-table-column prop="memUsage" label="内存使用率(%)" sortable>
     </el-table-column>
   </el-table>
-=======
-  <div>linux-tab</div>
->>>>>>> 6d2ddc1e02361f762cbb7d029a02f49ab90f2fb5:src/views/dashboard/resource/linuxTab.vue
 </template>
 
 <script>
@@ -42,7 +38,6 @@ import {
 } from "@/api/monitor/stats";
 
 export default {
-<<<<<<< HEAD:src/views/dashboard/resource/hostTab.vue
   name: "HostTab",
   data() {
     return {
@@ -92,7 +87,6 @@ export default {
                 nodeCPUUsage(qInstance),
                 nodeMemUsage(qInstance),
               ]).then((results) => {
-                console.log(results);
                 results[0]?.data?.result.map((item) => {
                   let data = hosts[item?.metric?.instance];
                   data["hostname"] = item?.metric?.nodename;
@@ -106,11 +100,6 @@ export default {
                   data["uptime"] = parseInt(item?.value[1]);
                   hosts[item?.metric?.instance] = data;
                 });
-=======
-  name:'LinuxTab'
-}
-</script>
->>>>>>> 6d2ddc1e02361f762cbb7d029a02f49ab90f2fb5:src/views/dashboard/resource/linuxTab.vue
 
                 results[2]?.data?.result.map((item) => {
                   let data = hosts[item?.metric?.instance];
